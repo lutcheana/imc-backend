75769c5858fdd0d61b52982d12b0a62135e78e14
renanlimabl <renanlimabl@gmail.com>
2020-12-17 15:21:19 -0300

    add mainpy on heroku

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
 import json
+import os
 app = Flask(__name__)
 CORS(app)
 
@@ -33,7 +34,7 @@ def index():
 
     
 
-    
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
